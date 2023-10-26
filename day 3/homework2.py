0b8b442b734c4d9739339bd13298339f3eba05c9
aleksandre2002 <145807536+aleksandre2002@users.noreply.github.com>
2023-10-26 12:33:59 +0400

    Add files via upload

---
+++ day 3/homework2.py
@@ -0,0 +1,17 @@
+#მომხმარებელს შემოატანინეთ თავისი სახელი, გვარი, ასაკი და დაპრინტეთ გრძელ წინადადებაში ყველა ეს მონაცემი.
+# მომხმარებელს შეეკითხეთ ასაკი, 
+#და დაუპრინტეთ 25 წელში რამდენი წლის  იქნება
+
+name = "ako"
+surname = "mitaishvili"
+age = 21
+
+name = input("what is your name")
+surname = input("what is your surname")
+age = input("how old are you")
+
+print(name + surname + age)
+
+
+
+
