8a25d4414a8313b93a9e4682645f4e0337d28524
aleksandre2002 <145807536+aleksandre2002@users.noreply.github.com>
2023-10-26 12:37:06 +0400

    fifth commit

---
+++ day 5/lesson5.py
@@ -0,0 +1,10 @@
+# მომხმარებელს კითხეთ თავისი და მამამისის ასაკი, 
+#დაუპრინტეთ ყოველ წელს რამდენჯერ უფროსი იქნება მამამისი მასზე.
+
+
+my_age = int(input("enter your age:")) #21
+fathers_age = int(input("enter your fathers name: ")) #50
+year = int(input("enter year:")) #2023
+
+for i in range(50)
+    print(str(year + i) + "წელს მამაჩემი ჩემზე იქნება " + str((fathers_age + i / (my_age + i)) +"ჯერ დიდი")
